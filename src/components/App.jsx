7d20874599a661525677783694988081ceee694b
YozefKubirya <106235869+YozefKubirya@users.noreply.github.com>
2023-07-12 13:36:37 +0300

    complete first task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,18 @@
+import user from '../assets/user.json'
+import { Profile } from './Profile/Profile';
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      На шо я жмякав в цьому React?
+      <Profile
+  key={user.tag}
+  username={user.username}
+  tag={user.tag}
+  location={user.location}
+  avatar={user.avatar}
+  stats={user.stats}
+/> 
+    </>
   );
 };
 // lkzxcjlkdsclkdmclkdsmcl
